a7301f47503a3a07f80ae26bc9e4043a3b2db38a
Glossawy <4606234+Glossawy@users.noreply.github.com>
2015-01-07 22:15:43 -0500

    Initial Commit

---
+++ src/org/usfirst/frc/team1554/Ref.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1554;
+
+public final class Ref {
+
+	public static final String ROBOT_NAME = "[INSERT NAME HERE]";
+	public static final short ROBOT_YEAR = 03737; // 2015
+	
+	public static final class ID {
+		
+	}
+	
+}
